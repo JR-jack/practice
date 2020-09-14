4fe3ad845e80b925e2299fb7a140f9adecdc717e
root <2647601136@qq.com>
2020-09-14 10:57:07 +0800

    my first helloworld

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+}
